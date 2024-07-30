3170e7793cf7794bcc9d0c667a240814174655b2
Edward J. Schwartz <edmcman@cmu.edu>
2024-07-30 10:37:53 -0400

    Add sample/eval script

---
+++ dirty/utils/sample_test.py
@@ -0,0 +1,145 @@
+# Sample a few test cases from the prediction json file.
+
+import json
+import argparse
+import multiprocessing
+import random
+import json
+import tempfile
+import os
+import shutil
+import subprocess
+import traceback
+import tqdm
+
+BIN_DIR = "/home/ed/Projects/DIRTY/dirt-binaries/all-binaries"
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Sample a few test cases from the dataset.")
+    parser.add_argument("-n", "--num_samples", type=int, default=50, help="Number of samples to be generated")
+
+    parser.add_argument("json_file", help="Path to the JSON file")
+    parser.add_argument("-o", "--output_file", help="Path to the output JSON file")
+    args = parser.parse_args()
+    json_file = args.json_file
+
+    with open(json_file, "r") as f:
+        data = json.load(f)
+
+    sampled_bins = random.sample(list(data.keys()), args.num_samples)
+
+    sampled_functions = list((bin,random.choice(list(data[bin].items()))) for bin in sampled_bins)
+
+    # if args.output_file:
+    #     with open(args.output_file, "w") as f:
+    #         json.dump(dict(sampled_functions), f, indent=4)
+    # else:
+    #     print(json.dumps(dict(sampled_functions), indent=4), end="\n")
+
+    def worker(input):
+        bin, (func, jsondata) = input
+        assert func.startswith("FUN_")
+        funcaddr = func.replace("FUN_", "0x")
+        #print(f"Bin: {bin}, Function: {func}")
+
+        temp_dir = tempfile.mkdtemp()
+        #print(f"Temporary directory: {temp_dir}")
+
+        temp_json_file = tempfile.NamedTemporaryFile(suffix=".json", delete=False)
+        temp_json_file_name = temp_json_file.name
+        temp_json_file.close()
+
+        # XXX Capture logs
+
+        symbol_log = subprocess.check_output([
+            "%s/support/analyzeHeadless" % os.environ["GHIDRA_INSTALL_DIR"],
+            temp_dir,
+            "DummyProject",
+            "-readOnly",
+            "-import",
+            os.path.join(BIN_DIR, bin),
+            "-postScript",
+            "/home/ed/Projects/DIRTY/DIRTY-ghidra/scripts/DIRTY_eval.py",
+            "/home/ed/Projects/DIRTY/DIRTY-ghidra/dirty/pred_mt.json",
+            bin,
+            funcaddr,
+            temp_json_file_name
+        ], stderr=subprocess.STDOUT)
+
+        with open(temp_json_file_name, "r") as temp_json_file:
+            symbol_data = json.loads(temp_json_file.read())
+
+        # Nothing to rewrite
+        del symbol_data['rewritten_decompilation']
+
+        #print("temp data: %s", str(temp_data))
+
+        # Copy the binary to a temporary file name
+        temp_bin_file = tempfile.NamedTemporaryFile(delete=False)
+        temp_bin_file_name = temp_bin_file.name
+        temp_bin_file.close()
+        shutil.copyfile(os.path.join(BIN_DIR, bin), temp_bin_file_name)
+
+        # Strip the binary
+        subprocess.run(["strip", temp_bin_file_name])
+
+        # Run Ghidra on the stripped version
+        # XXX Capture logs
+        strip_log = subprocess.check_output([
+            "%s/support/analyzeHeadless" % os.environ["GHIDRA_INSTALL_DIR"],
+            temp_dir,
+            "DummyProject",
+            "-readOnly",
+            "-import",
+            temp_bin_file_name,
+            "-postScript",
+            "/home/ed/Projects/DIRTY/DIRTY-ghidra/scripts/DIRTY_eval.py",
+            "/home/ed/Projects/DIRTY/DIRTY-ghidra/dirty/pred_mt.json",
+            bin,
+            funcaddr,
+            temp_json_file_name
+        ], stderr=subprocess.STDOUT)
+
+        with open(temp_json_file_name, "r") as temp_json_file:
+            strip_data = json.loads(temp_json_file.read())
+
+        # Delete the temporary binary file
+        os.remove(temp_bin_file_name)
+
+        # Delete the temporary file
+        os.remove(temp_json_file_name)
+
+        d = {"strip": strip_data, "symbol": symbol_data}
+
+        d["strip"]["aligned_vars"] = list(set(d["strip"]["vars"]) & set(jsondata.keys()))
+
+        d["strip"]["aligned_frac"] = len(d["strip"]["aligned_vars"]) / len(d["strip"]["vars"])
+
+
+        d["symbol"]["aligned_vars"] = list(set(d["symbol"]["vars"]) & {v[1] for v in jsondata.values()})
+        d["symbol"]["aligned_frac"] = len(d["symbol"]["aligned_vars"]) / len(d["symbol"]["vars"])
+
+        d["strip"]["log"] = strip_log.decode("utf-8")
+        d["symbol"]["log"] = symbol_log.decode("utf-8")
+
+        d["strip"]["aligned_frac"] = len(d["strip"]["aligned_vars"]) / len(d["strip"]["vars"])
+
+        return d
+    
+    def worker_catch(args):
+        try:
+            return worker(args)
+        except Exception as e:
+            traceback.print_exc()
+
+            return {"exception": str(e)}
+
+    with multiprocessing.Pool(4) as pool:
+        results = list(tqdm.tqdm(pool.imap_unordered(worker_catch, list(sampled_functions)), total=args.num_samples))
+
+    if args.output_file:
+        with open(args.output_file, "w") as f:
+            json.dump(results, f, indent=4)
+    else:
+        print(json.dumps(results, indent=4), end="\n")
---
+++ scripts/DIRTY_eval.py
@@ -0,0 +1,284 @@
+# Evaluation helper script for DIRTY Ghidra
+from functools import lru_cache
+from ghidra.app.decompiler import DecompInterface
+from ghidra.util.task import ConsoleTaskMonitor
+from ghidra.program.model.pcode import HighFunctionDBUtil
+from ghidra.program.model.symbol import SourceType
+from ghidra.program.model.data import PointerDataType, ArrayDataType, StructureDataType, UnionDataType, TypedefDataType
+from ghidra.app.services import DataTypeManagerService
+from ghidra.app.plugin.core.analysis import AutoAnalysisManager
+import json
+import random
+
+import ghidra_types
+
+debug = False
+
+def abort(s):
+    raise Exception(s)
+
+TYPELIB_PATH = "/home/ed/Downloads/typelib.json"
+
+codec = ghidra_types.TypeLibCodec()
+typelib = codec.decode(open(TYPELIB_PATH, "r").read())
+
+name_to_type = {}
+
+def all_typenames():
+    for _size, typelist in typelib.items():
+        for _freq, typeentry in typelist:
+            yield str(typeentry)
+
+def find_types_by_name(name):
+    for size, typelist in typelib.items():
+        for _freq, typeentry in typelist:
+            typename = str(typeentry)
+            #if size == 160000: # or "double" in typename:
+            #if debug and name in typename:
+            #    print(f"Hmm: {name} ==? {typename}")
+            if typename == name:
+                yield typeentry
+    return
+
+def find_type_by_name(name):
+    try:
+        return next(find_types_by_name(name))
+    except StopIteration:
+        print(f"Unable to find type {name} in typelib. Hopefully it is a built-in!")
+        return ghidra_types.TypeInfo(name=name, size=0)
+
+def find_type_in_ghidra_typemanager(name, dtm):
+    if dtm is None:
+        dtm = currentProgram().getDataTypeManager()
+
+    al = dtm.getAllDataTypes()
+    output_list = [dt for dt in al if dt.getName() == name]
+
+    if len(output_list) > 0:
+        return output_list[0]
+    else:
+        return None
+
+def find_type_in_any_ghidra_typemanager(name):
+    #dtms = [AutoAnalysisManager.getAnalysisManager(currentProgram()).getDataTypeManagerService()]
+    dtms = [currentProgram().getDataTypeManager()]
+    #dtms = state().getTool().getService(DataTypeManagerService).getDataTypeManagers()
+    for dtm in dtms:
+        output = find_type_in_ghidra_typemanager(name, dtm)
+        if output is not None:
+            return output
+    return None
+
+@lru_cache(maxsize=20000)
+def build_ghidra_type(typelib_type):
+    #print(f"build_ghidra_type {typelib_type} {typelib_type.__dict__} {type(typelib_type)}")
+
+    # First check our cache.  This is important for self-referential types
+    # (e.g., linked lists).
+    if str(typelib_type) in name_to_type:
+        return name_to_type[str(typelib_type)]
+
+    out = find_type_in_any_ghidra_typemanager(str(typelib_type))
+    if out is not None:
+        return out
+
+    if type(typelib_type) == ghidra_types.TypeInfo:
+
+        print(f"WARNING: {typelib_type.name} is a TypeInfo type: {typelib_type.debug}")
+        print(f"WARNING: Unable to find type {typelib_type.name} in Ghidra.")
+
+        match typelib_type.size:
+            case 1:
+                return find_type_in_any_ghidra_typemanager("byte")
+            case 4:
+                return find_type_in_any_ghidra_typemanager("uint32_t")
+            case 8:
+                return find_type_in_any_ghidra_typemanager("uint64_t")
+            case _:
+                abort(f"Unknown type with unusual size: {typelib_type.size} {typelib_type.name}")
+
+    elif type(typelib_type) == ghidra_types.Array:
+        element_type = build_ghidra_type(find_type_by_name(typelib_type.element_type))
+        return ArrayDataType(element_type, typelib_type.nelements, typelib_type.element_size)
+    elif type(typelib_type) == ghidra_types.Pointer:
+        target_type = build_ghidra_type(find_type_by_name(typelib_type.target_type_name))
+        return PointerDataType(target_type)
+        # Make type.
+    elif type(typelib_type) == ghidra_types.Struct or type(typelib_type) == ghidra_types.Union:
+        new_struct = StructureDataType(typelib_type.name, typelib_type.size) if type(typelib_type) == ghidra_types.Struct else UnionDataType(typelib_type.name)
+        # We need to immediately make this available in case we have a self-referential type.
+        name_to_type[str(typelib_type)] = new_struct
+        offset = 0
+        for member in (typelib_type.layout if type(typelib_type) == ghidra_types.Struct else typelib_type.members):
+            if type(member) == ghidra_types.UDT.Padding:
+                # Don't do anything?
+                pass
+                # new_struct.insertAtOffset(offset, VoidDataType(), member.size)
+            elif type(member) == ghidra_types.UDT.Field:
+                member_type = build_ghidra_type(find_type_by_name(member.type_name))
+                if type(typelib_type) == ghidra_types.Struct:
+                    new_struct.insertAtOffset(offset, member_type, member.size, member.name, "")
+                elif type(typelib_type) == ghidra_types.Union:
+                    new_struct.add(member_type, member.size, member.name, "")
+                else:
+                    abort("Unknown member type: " + str(type(typelib_type)))
+            else:
+                abort("Unknown member type: " + str(type(member)))
+
+            offset = offset + member.size
+                
+            #field_type = build_ghidra_type(find_type_by_name(field.type))
+            #new_struct.add(field_type, field.name, field.comment)
+        return new_struct
+    elif type(typelib_type) == ghidra_types.TypeDef:
+        other_type = build_ghidra_type(find_type_by_name(typelib_type.other_type_name))
+        return TypedefDataType(typelib_type.name, other_type)
+    else:
+        abort(f"Unknown type: {type(typelib_type)} {typelib_type}")
+
+def test_types():
+    total = 0
+    succ = 0
+
+    l = list(all_typenames())
+    random.shuffle(l)
+
+    #l = ["longlong[20][10]"]
+    #l = ["xen_string_string_map"]
+    #debug = True
+
+    for typename in l:
+        print(f"Trying to build type {typename}")
+        total += 1
+
+        if monitor().isCancelled():
+            break
+
+        monitor().setMessage(f"Building type {typename}")
+
+        try:
+            ti = find_type_by_name(typename)
+        except Exception as e:
+            print(f"Failed to find type {typename} exception: {e}")
+            continue
+
+        try:
+            gtype = build_ghidra_type(ti)
+            assert gtype is not None, "build_ghidra_type returned None."
+            print(f"Successfully built type {typename} in Ghidra: {gtype}")
+        except Exception as e:
+            print(f"Failed to build ghidra type {typename} exception: {e}")
+            #break
+            continue
+
+        succ += 1
+
+        print(f"Successfully built {succ}/{total} {float(succ)/total} types.")
+
+    exit(0)
+
+print ([x for x in getScriptArgs()])
+
+#exeName = currentProgram().getName()
+
+jsonFile = getScriptArgs()[0]
+exeName = getScriptArgs()[1]
+targetFunction = getScriptArgs()[2]
+targetFunctionAddr = int(targetFunction, 16)
+outputJsonFile = getScriptArgs()[3]
+
+#print("Using JSON file: " + jsonFile)
+#print("Target function: " + targetFunctionAddr)
+
+#jsonFile = askFile("Select JSON file", "Open")
+print(f"Parsing JSON for {exeName}")
+jsonObj = json.load(open(jsonFile))
+
+if exeName in jsonObj:
+    jsonObj = jsonObj[exeName]
+elif len(jsonObj) == 1:
+    jsonObj = jsonObj[list(jsonObj.keys())[0]]
+else:
+    abort(f"Unable to find the executable {exeName} in the JSON file.")
+
+#current_location = currentLocation()
+
+#for f in currentProgram().getFunctionManager().getFunctions(True):
+#    print(f.getName())
+
+func = next(f for f in currentProgram().getFunctionManager().getFunctions(True) if f.getEntryPoint().getOffset() == targetFunctionAddr)
+
+if func is None:
+    abort("Unable to find function " + targetFunction)
+
+print("Found target function: " + func.getName())
+
+assert func is not None
+
+funcName = f"FUN_%08x" % targetFunctionAddr
+if funcName in jsonObj:
+    jsonObj = jsonObj[funcName]
+else:
+    abort(f"Unable to find function %s in the JSON file." % funcName)
+
+ # Set up the decompiler
+decompiler = DecompInterface()
+decompiler.openProgram(func.getProgram())
+
+# Decompile the current function
+def decompile(func):
+    print("Decompiling function " + func.getName() + "...")
+    results = decompiler.decompileFunction(func, 0, ConsoleTaskMonitor())
+    if not results.decompileCompleted():
+        abort("Decompilation failed.")
+
+    # Get the high-level representation of the function
+    high_function = results.getHighFunction()
+    if not high_function:
+        abort("Failed to get high-level function representation.")
+
+    return results, high_function
+
+results, high_function = decompile(func)
+
+output = {}
+
+output['decompilation'] = results.getDecompiledFunction().getC()
+output['vars'] = [v.getName() for v in high_function.getLocalSymbolMap().getSymbols()]
+output['params'] = [v.getName() for v in high_function.getLocalSymbolMap().getSymbols() if v.isParameter()]
+
+# Rename variables (if no symbols)
+for var in high_function.getLocalSymbolMap().getSymbols():
+
+    original_name = var.getName()
+
+    if original_name in jsonObj:
+        new_type_name, new_name = jsonObj[original_name]
+        if new_type_name != "disappear" and new_type_name != "<unk>":
+            print("Renaming " + original_name + " to " + new_name + ".")
+
+            new_type = None
+
+            print(f"Attempting to retype {original_name}/{new_name} to {new_type_name}")
+
+            try:
+                ti = find_type_by_name(new_type_name)
+                new_type = build_ghidra_type(ti)
+                print(f"Changing type of {original_name}/{new_name} to {new_type_name}: {new_type}")
+            except Exception as e:
+                print(f"Failed to find or build type {new_type_name} exception: {e}")
+
+            HighFunctionDBUtil.updateDBVariable(var, new_name, new_type, SourceType.USER_DEFINED)
+
+
+        else:
+            print("Skipping disappear variable " + original_name + ".")
+    else:
+        print("No new name for " + original_name + " in JSON file.")
+
+# Re-decompile
+results, high_function = decompile(func)
+
+output['rewritten_decompilation'] = results.getDecompiledFunction().getC()
+
+json.dump(output, open(outputJsonFile, "w"), indent=4)
