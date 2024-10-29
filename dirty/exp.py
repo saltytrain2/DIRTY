d35dbbf0834b3bd586b11ab43b43c6cd80da943f
Edward J. Schwartz <moo.github.domain.edmcman@xoxy.net>
2024-10-29 17:28:09 -0400

    Estimate dataset length (#13)

--- dirty/exp.py
+++ dirty/exp.py
@@ -35,7 +35,7 @@ from torch.utils.data import DataLoader
 
 from model.model import TypeReconstructionModel
 from utils import util
-from utils.dataset import Dataset, WrappedLenDataset
+from utils.dataset import Dataset
 
 
 def train(args):
@@ -51,9 +51,7 @@ def train(args):
     train_set = Dataset(
         config["data"]["train_file"], config["data"], percent=float(args["--percent"])
     )
-    train_set = WrappedLenDataset(train_set)
     test_set = Dataset(config["data"]["test_file"], config["data"])
-    test_set = WrappedLenDataset(test_set)
     dev_set = Dataset(config["data"]["dev_file"], config["data"])
 
     print(f"len is {len(train_set)}")
