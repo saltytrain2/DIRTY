eaee541630284fc22a2cd2cb2d5f6f5d51e27a4d
Edward J. Schwartz <moo.github.domain.edmcman@xoxy.net>
2024-10-30 14:16:25 -0400

    Wandb improvements (#14)

--- dirty/exp.py
+++ dirty/exp.py
@@ -29,7 +29,7 @@ import wandb
 from docopt import docopt
 from pytorch_lightning import LightningDataModule
 from pytorch_lightning.loggers import WandbLogger
-from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
 from pytorch_lightning.tuner import Tuner
 from torch.utils.data import DataLoader
 
@@ -92,8 +92,10 @@ def train(args):
     # model
     model = TypeReconstructionModel(config)
 
-    wandb_logger = WandbLogger(name=args["--expname"], project="dire", log_model=True)
+    wandb_logger = WandbLogger(name=args["--expname"], project="dire", log_model="all")
     wandb_logger.log_hyperparams(config)
+    wandb_logger.watch(model, log="all")
+    monitor_var = "val_retype_acc" if config["data"]["retype"] else "val_rename_acc"
     resume_from_checkpoint = (
         args["--eval-ckpt"] if args["--eval-ckpt"] else args["--resume"]
     )
@@ -105,12 +107,16 @@ def train(args):
         gradient_clip_val=1,
         callbacks=[
             EarlyStopping(
-                monitor="val_retype_acc"
-                if config["data"]["retype"]
-                else "val_rename_acc",
+                monitor=monitor_var,
                 mode="max",
                 patience=config["train"]["patience"],
-            )
+            ),
+            # Save all checkpoints that improve accuracy
+            ModelCheckpoint(
+                monitor=monitor_var,
+                filename='{epoch}-{%s:.2f}' % monitor_var,
+                save_top_k=2,
+                mode="max")
         ],
         check_val_every_n_epoch=config["train"]["check_val_every_n_epoch"],
         accumulate_grad_batches=config["train"]["grad_accum_step"],
