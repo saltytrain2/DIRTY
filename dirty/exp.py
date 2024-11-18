ebcb70565d446aad83778e77b9c88e720c806e31
Edward J. Schwartz <edmcman@cmu.edu>
2024-11-18 15:36:03 -0500

    Use overall accuracy

--- dirty/exp.py
+++ dirty/exp.py
@@ -97,8 +97,8 @@ def train(args):
 
     wandb_logger = WandbLogger(name=args["--expname"], project="dire", log_model="all")
     wandb_logger.log_hyperparams(config)
-    wandb_logger.watch(model, log="all", log_freq=10000)
-    monitor_var = "val_retype_acc" if config["data"]["retype"] else "val_rename_acc"
+    wandb_logger.watch(model, log="parameters", log_freq=10000)
+    monitor_var = "val_acc"
     resume_from_checkpoint = (
         args["--eval-ckpt"] if args["--eval-ckpt"] else args["--resume"]
     )
